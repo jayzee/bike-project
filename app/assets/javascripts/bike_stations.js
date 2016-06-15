447995545c9a5f9d7f2e0bee23034935a189480c
Josh Zizmor <joshzizmor@gmail.com>
2016-06-15 16:20:25 -0400

    bug fixes

--- app/assets/javascripts/bike_stations.js
+++ app/assets/javascripts/bike_stations.js
@@ -37,6 +37,8 @@ $(document).ready(function(){
                    })
                 } // end of for loop
 
+                
+
             }
 
           })
--- app/controllers/bike_stations_controller.rb
+++ app/controllers/bike_stations_controller.rb
@@ -24,12 +24,11 @@ class BikeStationsController < ApplicationController
 
     @allStations = BikeStation.all
 
-    # parameters = {term: "restaurants", limit: 10, radius_filter: 100, latitude: 40.71911552 , longitude: -74.00666661}
-    #
-    # yelpFeedback = render json: Yelp.client.search("New York", parameters)
+    parameters = {term: "restaurants", limit: 10, radius_filter: 100, latitude: 40.71911552 , longitude: -74.00666661}
+    yelpFeedback = render json: Yelp.client.search("New York", parameters)
     binding.pry
     @return = { :error => false, :response => "Added"}
-    render json: @allStations
+    render json: {allStations: @allStations}
     # respond_to do |format|
     #   format.json { render json: @allStation, :status => :ok}
     # end
